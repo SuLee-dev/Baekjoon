9877b2fe18febfd7018bd1845f946bc48afe6b47
SuLee-dev <80202801+SuLee-dev@users.noreply.github.com>
2023-03-20 18:53:49 +0900

    Update 유기농 배추.js

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,5 +1,3 @@
-// 백준 2512번
-
 const fs = require("fs");
 
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
@@ -8,14 +6,10 @@ const T = input.shift();
 
 for (let i = 0; i < T; ++i) {
   const [M, N, K] = input.shift().split(" ").map(Number);
-  let farm = [];
-  let visited = [];
+  let farm = Array.from(Array(N), () => Array(M).fill(0));
+  let visited = Array.from(Array(N), () => Array(M).fill(false));
   let count = 0;
 
-  for (let j = 0; j < N; ++j) {
-    farm.push(Array(M).fill(0));
-    visited.push(Array(M).fill(0));
-  }
   for (let k = 0; k < K; ++k) {
     const [x, y] = input.shift().split(" ").map(Number);
     farm[y][x] = 1;
@@ -24,7 +18,7 @@ for (let i = 0; i < T; ++i) {
   function DFS(x, y) {
     if (x >= 0 && x < M && y >= 0 && y < N) {
       if (farm[y][x] && !visited[y][x]) {
-        visited[y][x] = 1;
+        visited[y][x] = true;
         DFS(x + 1, y);
         DFS(x, y + 1);
         DFS(x - 1, y);
