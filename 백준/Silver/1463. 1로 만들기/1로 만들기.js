f5fb3eab671c39baebc8da8c3dd4226b1506e37e
SuLee-dev <80202801+SuLee-dev@users.noreply.github.com>
2023-03-23 18:16:04 +0900

    Update 1로 만들기.js

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -2,13 +2,12 @@ const fs = require("fs");
 
 const N = Number(fs.readFileSync("/dev/stdin").toString().trim());
 
-let DP = [0, 0, 1, 1];
+let DP = [, 0];
 
-for (let i = 4; i <= N; ++i) {
-  if (!(i % 6)) DP[i] = Math.min(DP[i / 3], DP[i / 2], DP[i - 1]) + 1;
-  else if (!(i % 3)) DP[i] = Math.min(DP[i / 3], DP[i - 1]) + 1;
-  else if (!(i % 2)) DP[i] = Math.min(DP[i / 2], DP[i - 1]) + 1;
-  else DP[i] = DP[i - 1] + 1;
+for (let i = 2; i <= N; ++i) {
+  DP[i] = DP[i] = DP[i - 1] + 1;
+  if (!(i % 2)) DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+  if (!(i % 3)) DP[i] = Math.min(DP[i], DP[i / 3] + 1);
 }
 
 console.log(DP[N]);
